78a06fd419885f562ee5d9fb5b6d8176866fdd32
rajivsunar <rajivsunar07@gmail.com>
2021-07-17 17:45:14 +0545

    Sign up design logo added

--- app/src/main/java/com/RajivSunar/e_commercewebsite/LoginActivity.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/LoginActivity.kt
@@ -2,15 +2,33 @@ package com.RajivSunar.e_commercewebsite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
-    private lateinit var etPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var tvNoAccount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        tvNoAccount = findViewById(R.id.tvNoAccount)
+
+        btnLogin.setOnClickListener {
+
+        }
+
+        tvNoAccount.setOnClickListener {
+
+        }
+
+
     }
 }
