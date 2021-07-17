66d09ccf1419239cc088fe79ab163c6ed954d303
rajivsunar <rajivsunar07@gmail.com>
2021-07-17 18:36:06 +0545

    Sign Up activity binding

--- app/src/main/java/com/RajivSunar/e_commercewebsite/SignupActivity.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/SignupActivity.kt
@@ -2,11 +2,26 @@ package com.RajivSunar.e_commercewebsite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class SignupActivity : AppCompatActivity() {
+    private lateinit var etEmail: EditText
+    private lateinit var etName: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etConfirmPassword: EditText
+    private lateinit var btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        etEmail = findViewById(R.id.etEmail)
+        etName = findViewById(R.id.etName)
+        etPassword = findViewById(R.id.etPassword)
+        etConfirmPassword = findViewById(R.id.etConfirmPassword)
+        btnRegister = findViewById(R.id.btnRegister)
+
     }
 
 }
