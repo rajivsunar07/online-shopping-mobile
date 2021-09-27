dd13af7f50adc6badd3ea49e0ab659112c1a2c12
rajivsunar <rajivsunar07@gmail.com>
2021-09-27 22:33:09 +0545

    Gson to json and json to gson arraylist converters

---
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/entity/Converters.kt
@@ -0,0 +1,22 @@
+package com.RajivSunar.e_commercewebsite.data.entity
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+
+class Converters {
+        @TypeConverter
+        fun fromString(value: String?): ArrayList<String?>? {
+            val gson: Gson = GsonBuilder().create()
+            val listType = object : TypeToken<ArrayList<String>>() {}.type
+            return gson.fromJson(value, listType)
+        }
+
+        @TypeConverter
+        fun fromArrayList(list: ArrayList<String?>?): String {
+            val type = object: TypeToken<ArrayList<String?>?>() {}.type
+            return Gson().toJson(list, type)
+        }
+
+}
