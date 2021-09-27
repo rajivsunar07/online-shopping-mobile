ad2333d053325a0f457e6839efaf192cf709dc2b
rajivsunar <rajivsunar07@gmail.com>
2021-09-27 22:36:37 +0545

    Header interceptor added for api

---
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/api/MyInterceptor.kt
@@ -0,0 +1,14 @@
+package com.RajivSunar.e_commercewebsite.data.api
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class MyInterceptor: Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+            .newBuilder()
+            .addHeader("Authorization", "${ServiceBuilder.token}")
+            .build()
+        return chain.proceed(request)
+    }
+}
