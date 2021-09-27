de2d00361fb9a87ba4dcf931cb87486aff546705
rajivsunar <rajivsunar07@gmail.com>
2021-09-27 22:38:47 +0545

    Order entity created

---
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/entity/Order.kt
@@ -0,0 +1,24 @@
+package com.RajivSunar.e_commercewebsite.data.entity
+
+import androidx.room.*
+import org.jetbrains.annotations.NotNull
+import com.google.gson.Gson
+
+import com.google.gson.reflect.TypeToken
+import java.util.*
+import kotlin.collections.ArrayList
+
+@Entity(indices = [Index(value = ["_id"], unique = true)])
+data class Order(
+    var item: ArrayList<String>? = null,
+    var total_price: Int? = null,
+    var user: String? = null,
+    var status: String? = null,
+    var checkout: String? = null,
+    var created_at: String? = null,
+    var updated_at: String? = null
+    ) {
+    @PrimaryKey(autoGenerate = false)
+    @NotNull
+    var _id: String = ""
+}
