0020109c4862a02c78b4dae12dfac97ffccfe76b
rajivsunar <rajivsunar07@gmail.com>
2021-09-29 17:13:19 +0545

    On image click intent added

--- app/src/main/java/com/RajivSunar/e_commercewebsite/ui/adapter/ProductAdapter.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/ui/adapter/ProductAdapter.kt
@@ -1,6 +1,7 @@
 package com.RajivSunar.e_commercewebsite.ui.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,11 +9,13 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.RajivSunar.e_commercewebsite.R
 import com.RajivSunar.e_commercewebsite.data.api.ServiceBuilder
 import com.RajivSunar.e_commercewebsite.data.entity.Product
 import com.RajivSunar.e_commercewebsite.data.repository.OrderRepository
+import com.RajivSunar.e_commercewebsite.ui.product.ProductDetailActivity
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -51,6 +54,12 @@ class ProductAdapter(
             holder.tvName.text = product.name
             holder.tvPrice.text = "RS. " + product.price.toString()
 
+            holder.imgProduct.setOnClickListener{
+                val intent = Intent(this.context, ProductDetailActivity::class.java)
+                intent.putExtra("id", product._id)
+                context.startActivity(intent)
+            }
+
             holder.btnAddToCart.setOnClickListener {
 
                 CoroutineScope(Dispatchers.IO).launch {
@@ -79,6 +88,7 @@ class ProductAdapter(
         }
 
     }
+
 
     override fun getItemCount(): Int {
         return productList.size
