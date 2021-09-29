075fa87b56287dc384e29bc36217dded473075a2
rajivsunar <rajivsunar07@gmail.com>
2021-09-29 17:10:29 +0545

    2 functions removed

--- app/src/main/java/com/RajivSunar/e_commercewebsite/data/dao/OrderDAO.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/dao/OrderDAO.kt
@@ -1,18 +1,21 @@
 package com.RajivSunar.e_commercewebsite.data.dao
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.RajivSunar.e_commercewebsite.data.entity.Order
+import com.RajivSunar.e_commercewebsite.data.entity.OrderItem
 
+@Dao
 interface OrderDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(order: Order)
 
-    @Query("Select * from Order")
-    suspend fun getAllOrders():List<Order>
+//   Order @Query("Select item from Order where _id=:id")
+//    suspend fun getOrder(id: String): List<OrderItem>
 
-    @Query("Select count(*) from Order")
-    suspend fun getCount(): Int?
+//    @Query("Select count(*) from Order")
+//    suspend fun getCount(): Int?
 }
