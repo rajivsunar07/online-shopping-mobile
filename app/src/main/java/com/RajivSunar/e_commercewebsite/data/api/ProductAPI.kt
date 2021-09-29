5c5a0148555119dae7c26cb59ea6834e8e071fe0
rajivsunar <rajivsunar07@gmail.com>
2021-09-29 17:13:37 +0545

    Get one, get for user and add product functions added

--- app/src/main/java/com/RajivSunar/e_commercewebsite/data/api/ProductAPI.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/api/ProductAPI.kt
@@ -1,6 +1,8 @@
 package com.RajivSunar.e_commercewebsite.data.api
 
 import com.RajivSunar.e_commercewebsite.data.response.ProductResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -9,4 +11,22 @@ interface ProductAPI {
     @GET("product/")
     suspend fun getAll(): Response<ProductResponse>
 
+    @GET("product/{id}")
+    suspend fun getOne(
+        @Path("id") id: String
+        ): Response<ProductResponse>
+
+
+    @GET("product/user/all")
+    suspend fun getForUser(): Response<ProductResponse>
+
+    @Multipart
+    @POST("product/")
+    suspend fun addProduct(
+        @Part image: ArrayList<MultipartBody.Part>,
+        @Part("name") name: RequestBody,
+        @Part("description") description: RequestBody,
+        @Part("price") price: Int,
+        @Part("for") _for: RequestBody
+    ): Response<ProductResponse>
 }
