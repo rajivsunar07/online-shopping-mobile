76c75a103c863e44103cac62ee63811ce8989494
rajivsunar <rajivsunar07@gmail.com>
2021-10-01 14:50:58 +0545

    Message field added

--- app/src/main/java/com/RajivSunar/e_commercewebsite/data/response/ProductResponse.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/response/ProductResponse.kt
@@ -4,5 +4,6 @@ import com.RajivSunar.e_commercewebsite.data.entity.Product
 
 data class ProductResponse (
     val success: Boolean? = null,
-    val result: List<Product>? = null
+    val result: List<Product>? = null,
+    val message: String? = null
 )
