c8ccdb98edec4678b82914669a5e68741a4b3542
rajivsunar <rajivsunar07@gmail.com>
2021-10-01 14:49:29 +0545

    User response user and message field added

--- app/src/main/java/com/RajivSunar/e_commercewebsite/data/response/UserResponse.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/response/UserResponse.kt
@@ -1,6 +1,10 @@
 package com.RajivSunar.e_commercewebsite.data.response
 
+import com.RajivSunar.e_commercewebsite.data.entity.User
+
 class UserResponse (
     val success: Boolean? = null,
-    val token: String? = null
+    val token: String? = null,
+    val user: User? = null,
+    val message: String? = null
 )
