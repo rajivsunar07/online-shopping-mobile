27aa3cbf5bfc73d5129b69f6e1dfae90d600872c
rajivsunar <rajivsunar07@gmail.com>
2021-10-01 14:49:51 +0545

    Get user, update user and changepassword function added

--- app/src/main/java/com/RajivSunar/e_commercewebsite/data/api/UserAPI.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/api/UserAPI.kt
@@ -2,11 +2,10 @@ package com.RajivSunar.e_commercewebsite.data.api
 
 import com.RajivSunar.e_commercewebsite.data.entity.User
 import com.RajivSunar.e_commercewebsite.data.response.UserResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface UserAPI {
 
@@ -21,4 +20,25 @@ interface UserAPI {
         @Field("email") email: String,
         @Field("password") password: String
     ): Response<UserResponse>
+
+    @GET("user/")
+    suspend fun getUser(): Response<UserResponse>
+
+    @Multipart
+    @PATCH("user/")
+    suspend fun updateUser(
+        @Part image: MultipartBody.Part,
+        @Part("email") email: RequestBody,
+        @Part("name") name: RequestBody,
+        @Part("address") address: RequestBody,
+        @Part("phone") phone: RequestBody
+    ): Response<UserResponse>
+
+    @FormUrlEncoded
+    @PATCH("user/password")
+    suspend fun changePassword(
+        @Field("password") password: String,
+        @Field("newPassword") newPassword: String
+    ): Response<UserResponse>
+
 }
