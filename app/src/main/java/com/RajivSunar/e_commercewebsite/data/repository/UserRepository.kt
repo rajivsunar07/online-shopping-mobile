e5d8ca87109f11c87fd9251d31afa0bce0ca715b
rajivsunar <rajivsunar07@gmail.com>
2021-10-01 14:50:01 +0545

    Get user, update user and changepassword function added for repository

--- app/src/main/java/com/RajivSunar/e_commercewebsite/data/repository/UserRepository.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/repository/UserRepository.kt
@@ -5,6 +5,8 @@ import com.RajivSunar.e_commercewebsite.data.api.ServiceBuilder
 import com.RajivSunar.e_commercewebsite.data.api.UserAPI
 import com.RajivSunar.e_commercewebsite.data.entity.User
 import com.RajivSunar.e_commercewebsite.data.response.UserResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 class UserRepository: MyApiRequest() {
     private val userApi =
@@ -19,6 +21,36 @@ class UserRepository: MyApiRequest() {
     suspend fun login(username: String, password: String): UserResponse{
         return apiRequest {
             userApi.login(username, password)
+        }
+    }
+
+    suspend fun getUser(): UserResponse{
+        return apiRequest {
+            userApi.getUser()
+        }
+    }
+
+    suspend fun updateUser(
+        image: MultipartBody.Part,
+        email: RequestBody,
+        name: RequestBody,
+        phone: RequestBody,
+        address: RequestBody
+    ): UserResponse{
+        return apiRequest {
+            userApi.updateUser(
+                image = image,
+                email = email,
+                name = name,
+                phone= phone,
+                address = address
+            )
+        }
+    }
+
+    suspend fun changePassword(password: String, newPassword: String): UserResponse{
+        return apiRequest {
+            userApi.changePassword(password, newPassword)
         }
     }
 }
