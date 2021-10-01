c8d0d27cb1be9f3eecbf97e546cfbb4b1345b547
rajivsunar <rajivsunar07@gmail.com>
2021-10-01 14:50:11 +0545

    Made parcelable

--- app/src/main/java/com/RajivSunar/e_commercewebsite/data/entity/User.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/data/entity/User.kt
@@ -1,5 +1,7 @@
 package com.RajivSunar.e_commercewebsite.data.entity
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import org.jetbrains.annotations.NotNull
@@ -8,9 +10,47 @@ import org.jetbrains.annotations.NotNull
 data class User(
     var email: String? = null,
     var name: String? = null,
-    var password: String? = null
-) {
+    var password: String? = null,
+    var address: String? = null,
+    var phone: String? = null,
+    var image: String? = null
+): Parcelable {
     @PrimaryKey(autoGenerate = false)
     @NotNull
     var _id: String = ""
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+        _id = parcel.readString().toString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(email)
+        parcel.writeString(name)
+        parcel.writeString(password)
+        parcel.writeString(address)
+        parcel.writeString(phone)
+        parcel.writeString(image)
+        parcel.writeString(_id)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
