28b219ec91b17d74f44ac4d5f9662cac3fdefb4b
rajivsunar <rajivsunar07@gmail.com>
2021-10-02 10:45:24 +0545

    Splash screen added

--- app/src/main/java/com/RajivSunar/e_commercewebsite/ui/MainActivity.kt
+++ app/src/main/java/com/RajivSunar/e_commercewebsite/ui/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.RajivSunar.e_commercewebsite.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.RajivSunar.e_commercewebsite.R
+import com.RajivSunar.e_commercewebsite.ui.product.ProductActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, ProductActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
